8ba6fd7af90ddd7b52ca042d03fdc3ba481c29b1
kazahat <tamelant@gmail.com>
2024-03-22 20:02:23 +0400

    add all const

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init()
 
@@ -7,7 +8,22 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Tire Game")
-icon = pygame.image.load('icon.png')
+icon = pygame.image.load('image/icon.png')
+pygame.display.set_icon(icon)
+
+
+target_image = pygame.image.load('image/target.png')
+target_width = 71
+target_height = 50
+
+target_x = random.randint(0, SCREEN_WIDTH - target_width)
+target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+color = random.choice(['blue', 'green', 'red', 'yellow', 'purple', 'orange'])
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
 
 running = True
