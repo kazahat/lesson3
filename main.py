99dc68c013de0dddcdc5dbb4c8bdb92be30e88ed
kazahat <tamelant@gmail.com>
2024-03-22 20:37:40 +0400

    add all cicle code

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ SCREEN_HEIGHT = 600
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Tire Game")
-icon = pygame.image.load('image/icon.png')
+icon = pygame.image.load('image/icon.jpg')
 pygame.display.set_icon(icon)
 
 
@@ -26,5 +26,20 @@ color = random.choice(['blue', 'green', 'red', 'yellow', 'purple', 'orange'])
 
 running = True
 while running:
-    pass
+    screen.fill(color)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            mouse_x, mouse_y = pygame.mouse.get_pos()
+            if target_x <= mouse_x <= target_x + target_width and target_y <= mouse_y <= target_y + target_height:
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+    screen.blit(target_image, (target_x, target_y))
+    pygame.display.update()
+
+
+
+
 pygame.quit()
