4b3ad11baeec6e17602fdd6a00c786e613b4e4ae
kazahat <tamelant@gmail.com>
2024-03-22 21:03:13 +0400

    dom zadanie

--- main.py
+++ main.py
@@ -25,15 +25,20 @@ color = random.choice(['blue', 'green', 'red', 'yellow', 'purple', 'orange'])
 
 
 running = True
+tire_count = 0
 while running:
     screen.fill(color)
-
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             mouse_x, mouse_y = pygame.mouse.get_pos()
+
             if target_x <= mouse_x <= target_x + target_width and target_y <= mouse_y <= target_y + target_height:
+                tire_count += 1
+                target_x = random.randint(0, SCREEN_WIDTH - target_width)
+                target_y = random.randint(0, SCREEN_HEIGHT - target_height)
+            else:
                 target_x = random.randint(0, SCREEN_WIDTH - target_width)
                 target_y = random.randint(0, SCREEN_HEIGHT - target_height)
     screen.blit(target_image, (target_x, target_y))
@@ -43,3 +48,4 @@ while running:
 
 
 pygame.quit()
+print(f"Вы попали в мишень {tire_count} раз")
